e0ecb9fb1089f90f67683c80b24c59ca9dc5432c
Zhongheng-Cheng <zc2737@columbia.edu>
2024-04-10 11:29:35 -0400

    added go-to function

--- source/visual_debugger/debugger.py
+++ source/visual_debugger/debugger.py
@@ -14,11 +14,21 @@ class Cycle():
             self.now = 999
         return
 
+    def move_to_cycle(self, num):
+        self.now = num
+        if self.now < self.limit[0]:
+            self.now = 0
+        elif self.now > self.limit[1]:
+            self.now = 999
+        return
+
 def new_window(title, nlines, ncols, begin_y, begin_x):
     win = curses.newwin(nlines, ncols, begin_y, begin_x)
     win.border()
-    win.addstr(0, max(begin_x, (begin_x + ncols) // 2 - len(title) // 2), title)
+    win.addstr(0, max(begin_x, (begin_x + ncols) // 2 - len(title) // 2), title, curses.A_BOLD | curses.COLOR_RED)
+    win.refresh()
     return win
+
 
 def main(stdscr):
     
@@ -28,13 +38,18 @@ def main(stdscr):
     stdscr.clear()
     stdscr.refresh()
 
+    goto_input = ""
+    recording = False
+
     # create window for Keys
     win_keys = new_window(title="Keys", nlines=10, ncols=15, begin_y=0, begin_x=0)
     win_keys.addstr(1, 1, "Q: quit")
-    win_keys.addstr(2, 1, "V: cycle-10")
-    win_keys.addstr(3, 1, "B: cycle-1")
-    win_keys.addstr(4, 1, "N: cycle+1")
-    win_keys.addstr(5, 1, "M: cycle+10")
+    win_keys.addstr(2, 1, "→: next cycle")
+    win_keys.addstr(3, 1, "←: prev cycle")
+    win_keys.addstr(4, 1, "↑: +10 cycles")
+    win_keys.addstr(5, 1, "↓: -10 cycles")
+    win_keys.addstr(6, 1, "G: go to     ")
+    win_keys.addstr(7, 1, "T: end go to ")
     win_keys.refresh()
 
 
@@ -42,20 +57,45 @@ def main(stdscr):
     while True:
         stdscr.addstr(0, 17, f"Cycle: {cycle.now:3d}")
         stdscr.refresh()
+        win_keys.refresh()
 
-        key_press = chr(stdscr.getch())
-        if key_press == "q":
-            # quit the debugger
-            break
-        elif key_press == 'v':   
-            cycle.add_to_cycle(-10)
-        elif key_press == 'b':   
-            cycle.add_to_cycle(-1)
-        elif key_press == 'n':   
-            cycle.add_to_cycle(1)
-        elif key_press == 'm':   
-            cycle.add_to_cycle(10)
+        key_press = stdscr.getch()
+
+        # quit the debugger
+        if key_press == ord('q'):
+            return
         
+        # move cycle count
+        elif key_press == curses.KEY_RIGHT:   
+            cycle.add_to_cycle(1)
+        elif key_press == curses.KEY_LEFT:   
+            cycle.add_to_cycle(-1)
+        elif key_press == curses.KEY_UP:   
+            cycle.add_to_cycle(10)
+        elif key_press == curses.KEY_DOWN:   
+            cycle.add_to_cycle(-10)
 
+        # go-to function
+        elif key_press == ord('g'):
+            recording = True
+            win_keys.addstr(6, 1, "G: go to     ", curses.A_REVERSE)
+        elif recording and key_press == 10: # ENTER
+            recording = False
+            win_keys.addstr(6, 1, "G: go to     ")
+            if goto_input:
+                cycle.move_to_cycle(int(goto_input[-3:]))
+                goto_input = ""
+        elif recording and key_press == 263: # BACKSPACE
+            goto_input = goto_input[:-1]
+            win_keys.addstr(6, 1, f"G: go to {goto_input[-3:]:3s}", curses.A_REVERSE)
+        elif recording and 48 <= key_press <= 57: # numbers: 0-9
+            char = str(int(chr(key_press)))
+            goto_input += char
+            win_keys.addstr(6, 1, f"G: go to {goto_input[-3:]:3s}", curses.A_REVERSE)
+
+
+        stdscr.addstr(20, 1, f"Key pressed: {str(key_press):3s}")
+        stdscr.refresh()
+        win_keys.refresh()
 
 curses.wrapper(main)
