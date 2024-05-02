d1d3357884653244c79478b7e06e8cdc29b1f2c2
Zhongheng-Cheng <zc2737@columbia.edu>
2024-05-02 15:49:13 -0400

    updated auto running all the xxx.out

--- Files/auto_test.sh
+++ Files/auto_test.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-GROUND_TRUTH_DIR=~/csee4824/4824-labs/lab4/project3_ground_truth
-TEST_DIR=~/csee4824/4824-projects/project3/source
+# GROUND_TRUTH_DIR=~/csee4824/4824-labs/lab4/project3_ground_truth
+TEST_DIR=$(pwd)
 # TEST_DIR=${GROUND_TRUTH_DIR}
 
-if [ -d "${GROUND_TRUTH_DIR}/output" ]; then
-    echo "Ground Truth files exist"
-else
-    echo "Generating Ground Truth files"
-    ./generate_ground_truth.sh
-fi
+export VCS_LIC_EXPIRE_WARNING=0
 
-echo "Comparing ground truth outputs to new processor"
+# if [ -d "${GROUND_TRUTH_DIR}/output" ]; then
+#     echo "Ground Truth files exist"
+# else
+#     echo "Generating Ground Truth files"
+#     ./generate_ground_truth.sh
+# fi
 
-cd ${TEST_DIR}
-source setup-paths.sh
+# echo "Comparing ground truth outputs to new processor"
+
+# cd ${TEST_DIR}
+# source setup-paths.sh
+
 
 for source_file in programs/*.{s,c}; do
     if [ "${source_file}" = "programs/crt.s" ]; then
@@ -28,27 +31,27 @@ for source_file in programs/*.{s,c}; do
 
     # Writeback output Test
     
-    # compare the test output and ground truth output
-    diff ${TEST_DIR}/output/${program}.wb ${GROUND_TRUTH_DIR}/output/${program}.wb > /dev/null
-    if [ $? -eq 0 ]; then
-        echo -e "writeback test: \e[32mPASS\e[0m"
-    else
-        echo -e "writeback test: \e[31mFAIL\e[0m"
-    fi
+    # # compare the test output and ground truth output
+    # diff ${TEST_DIR}/output/${program}.wb ${GROUND_TRUTH_DIR}/output/${program}.wb > /dev/null
+    # if [ $? -eq 0 ]; then
+    #     echo -e "writeback test: \e[32mPASS\e[0m"
+    # else
+    #     echo -e "writeback test: \e[31mFAIL\e[0m"
+    # fi
 
-    # Memory output Test
+    # # Memory output Test
 
-    # search for all lines start with "@@@"
-    cat ${GROUND_TRUTH_DIR}/output/${program}.out | grep "@@@" > ground_truth_output.tmp
-    cat ${TEST_DIR}/output/${program}.out | grep "@@@" > test_output.tmp
+    # # search for all lines start with "@@@"
+    # cat ${GROUND_TRUTH_DIR}/output/${program}.out | grep "@@@" > ground_truth_output.tmp
+    # cat ${TEST_DIR}/output/${program}.out | grep "@@@" > test_output.tmp
 
-    # compare the memory outputs of test and ground truth
-    diff ground_truth_output.tmp test_output.tmp > /dev/null
-    if [ $? -eq 0 ]; then
-        echo -e "memory test:    \e[32mPASS\e[0m"
-    else
-        echo -e "memory test:    \e[31mFAIL\e[0m"
-    fi
+    # # compare the memory outputs of test and ground truth
+    # diff ground_truth_output.tmp test_output.tmp > /dev/null
+    # if [ $? -eq 0 ]; then
+    #     echo -e "memory test:    \e[32mPASS\e[0m"
+    # else
+    #     echo -e "memory test:    \e[31mFAIL\e[0m"
+    # fi
 
 done
 
